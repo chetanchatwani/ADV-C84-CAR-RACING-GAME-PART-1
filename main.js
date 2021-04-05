5ddb44140be815cb84733afccd1edbef76983e97
Chetan Chatwani <chetanchatwani@gmail.com>
2021-04-05 15:53:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ ctx = canvas.getContext("2d");
 
 car1_width=120;
 car1_height=70;
-car1_image="images/car1.png"
+car1_image="images/car1.png";
 car1_x=10;
 car1_y=10;
 
 car2_width=120;
 car2_height=70;
-car2_image="images/car2.png"
+car2_image="images/car2.png";
 car2_x=10;
 car2_y=100;
 
@@ -26,11 +26,11 @@ function add() {
 	background_imgTag.src = background_image;   // load image
 
 	car1_imgTag = new Image(); //defining a variable with a new image
-	car1imgTag.onload = uploadcar1; // setting a function, onloading this variable
+	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
 	car1_imgTag.src = car1_image;   // load image
 
 	car2_imgTag = new Image(); //defining a variable with a new image
-	car2imgTag.onload = uploadcar2 // setting a function, onloading this variable
+	car2_imgTag.onload = uploadcar2 // setting a function, onloading this variable
 	car2_imgTag.src = car2_image;   // load image
 
 }
